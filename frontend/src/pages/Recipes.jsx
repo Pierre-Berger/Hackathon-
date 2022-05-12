682faf2b282f023ffc91bceb5cc634e766a47f84
BERTELET <breoberteletpolo@gmail.com>
2022-05-12 15:55:54 +0200

    oups je taffais sur dev

--- frontend/src/pages/Recipes.jsx
+++ frontend/src/pages/Recipes.jsx
@@ -1,35 +1,39 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 import RecipeCard from "../components/RecipeCard";
 import "../css/recipe.css";
+import recipe from "../components/Recipe";
 
 function Recipes() {
   return (
-    <div className="recipe-font">
-      <div className="recipe-cont">
-        {" "}
-        <h3 className="recipe-slider-title"> SUCREE </h3>
-        <div className="recipes-slider">
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-        </div>
-        <h3 className="recipe-slider-title"> SALE </h3>
-        <div className="recipes-slider">
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-        </div>
-        <h3 className="recipe-slider-title"> SAVEUR FRAMBOISE </h3>
-        <div className="recipes-slider">
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
+    <>
+      <Navbar />
+      <div className="recipe-font">
+        <div className="recipe-cont">
+          {" "}
+          <h3 className="recipe-slider-title"> SUCREE </h3>
+          <div className="recipes-slider">
+            {recipe.map((r) => (
+              <RecipeCard recipe={r} />
+            ))}
+          </div>
+          <h3 className="recipe-slider-title"> SALE </h3>
+          <div className="recipes-slider">
+            <RecipeCard recipe={recipe} />
+            <RecipeCard recipe={recipe} />
+            <RecipeCard recipe={recipe} />
+            <RecipeCard recipe={recipe} />
+          </div>
+          <h3 className="recipe-slider-title"> SAVEUR FRAMBOISE </h3>
+          <div className="recipes-slider">
+            <RecipeCard recipe={recipe} />
+            <RecipeCard recipe={recipe} />
+            <RecipeCard recipe={recipe} />
+            <RecipeCard recipe={recipe} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
