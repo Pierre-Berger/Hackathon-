414bb5abc5c00461cc3b74a445414968a3d8e906
Aristotia <antoine.belloni@gmail.com>
2022-05-12 15:18:51 +0200

    CatnipItem first render done

--- frontend/src/pages/CatnipItem.jsx
+++ frontend/src/pages/CatnipItem.jsx
@@ -1,26 +1,32 @@
 import catnipItems from "../assets/catnipItems";
+import "../css/catnipItem.css";
 
 export default function CatnipItem() {
   return (
     <div>
       <div className="catnipItem-article">
-        <h1 className="catnipItem-title">{catnipItems[0].title}</h1>
+        <h1 className="catnipItem-title">{catnipItems[3].title}</h1>
         <img
           className="catnipItem-image"
-          src={catnipItems[0].image}
-          alt={catnipItems[0].title}
+          src={catnipItems[3].image}
+          alt={catnipItems[3].title}
         />
         <div className="catnipItem-description">
           <ul>
-            {catnipItems[0].description.map((el) => (
+            {catnipItems[3].description.map((el) => (
               <li>{el}</li>
             ))}
           </ul>
         </div>
         <div className="catnipItem-information">
-          {/* <div className="catnipItem-stars">{drawStars}</div> */}
-          <div className="catnipItem-price">{catnipItems[0].price} €</div>
+          <div className="catnipItem-stars">
+            {"★".repeat(catnipItems[3].starNumber)}/5
+          </div>
+          <div className="catnipItem-price">{catnipItems[3].price} €</div>
         </div>
+        <button className="catnipItem-button" type="button">
+          Ajouter au panier
+        </button>
       </div>
     </div>
   );
