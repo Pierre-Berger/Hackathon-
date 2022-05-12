a1cad96da809654fe9f13b823ec754c3e317873f
Aristotia <antoine.belloni@gmail.com>
2022-05-12 20:33:48 +0200

    empty cart problem solved

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -12,23 +12,23 @@ export default function Cart({ catnipItems, cart, updateCart, idItem }) {
     updateCart(cart + 1);
   };
   console.log(kaaris);
-  return isOpen && kaaris ? (
+  return isOpen ? (
     <div className="cart-component">
       <CartBubble setisOpen={setisOpen} isOpen={isOpen} />
       <h2>Panier</h2>
-      <div className="cart-content-container">
-        <div className="cart-content">
-          <div className="article-cart">
-            <h3>Articles</h3>
-            <div>{`${kaaris ? kaaris.title : null} : ${
-              kaaris ? kaaris.price : null
-            }€`}</div>
-          </div>
-          <div className="quantity-cart">
-            <h3>Quantité</h3>
-            <div>{cart}</div>
-          </div>
-          {cart > 0 ? (
+      {cart > 0 ? (
+        <div className="cart-content-container">
+          <div className="cart-content">
+            <div className="article-cart">
+              <h3>Articles</h3>
+              <div>{`${kaaris ? kaaris.title : null} : ${
+                kaaris ? kaaris.price : null
+              }€`}</div>
+            </div>
+            <div className="quantity-cart">
+              <h3>Quantité</h3>
+              <div>{cart}</div>
+            </div>
             <div className="modif">
               <h3>- / +</h3>
               <div>
@@ -40,11 +40,15 @@ export default function Cart({ catnipItems, cart, updateCart, idItem }) {
                 </button>
               </div>
             </div>
-          ) : null}
-        </div>
+          </div>
 
-        <h3 className="total">{`Total : ${kaaris.price * cart}€`}</h3>
-      </div>
+          <h3 className="total">{`Total : ${kaaris.price * cart}€`}</h3>
+        </div>
+      ) : (
+        <div>
+          <h3 className="empty-cart">Votre panier est vide !</h3>
+        </div>
+      )}
     </div>
   ) : (
     <CartBubble setisOpen={setisOpen} isOpen={isOpen} />
