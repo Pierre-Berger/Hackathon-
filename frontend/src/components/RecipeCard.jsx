9a0906f11c5c5b75f8faaf36f758d1add308e3d1
BERTELET <breoberteletpolo@gmail.com>
2022-05-12 16:31:29 +0200

    torched the recipe menu

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function RecipeCard() {
+function RecipeCard({ recipe }) {
   return (
     <div className="recipe-card">
       <div className="image-de-plat">
-        <img src="../src/assets/images/kaaris.jpg" alt="BOUFFE POUR CLEBS" />
+        <img src={recipe.image} alt="BOUFFE POUR CLEBS" />
       </div>
-      <div> le zizi ainsi que le caca </div>
+      <div> {recipe.title} </div>
     </div>
   );
 }
--- frontend/src/pages/Recipes.jsx
+++ frontend/src/pages/Recipes.jsx
@@ -5,31 +5,50 @@ import "../css/recipe.css";
 import recipe from "../components/Recipe";
 
 function Recipes() {
+  const fish = recipe.filter((f) => f.ingredientsFilter.fish);
+  const meat = recipe.filter((f) => f.ingredientsFilter.meat);
+  const milk = recipe.filter((f) => f.ingredientsFilter.milk);
+  const weed = recipe.filter((f) => f.ingredientsFilter.weed);
+  const healthy = recipe.filter((f) => f.ingredientsFilter.healthy);
+
   return (
     <>
       <Navbar />
       <div className="recipe-font">
         <div className="recipe-cont">
           {" "}
-          <h3 className="recipe-slider-title"> SUCREE </h3>
+          <h3 className="recipe-slider-title"> Recettes au poisson ! </h3>
           <div className="recipes-slider">
-            {recipe.map((r) => (
+            {fish.map((r) => (
               <RecipeCard recipe={r} />
             ))}
           </div>
-          <h3 className="recipe-slider-title"> SALE </h3>
+          <h3 className="recipe-slider-title"> Recettes de viandard ! </h3>
           <div className="recipes-slider">
-            <RecipeCard recipe={recipe} />
-            <RecipeCard recipe={recipe} />
-            <RecipeCard recipe={recipe} />
-            <RecipeCard recipe={recipe} />
+            {meat.map((r) => (
+              <RecipeCard recipe={r} />
+            ))}
           </div>
-          <h3 className="recipe-slider-title"> SAVEUR FRAMBOISE </h3>
+          <h3 className="recipe-slider-title"> Recettes contenant du lait </h3>
           <div className="recipes-slider">
-            <RecipeCard recipe={recipe} />
-            <RecipeCard recipe={recipe} />
-            <RecipeCard recipe={recipe} />
-            <RecipeCard recipe={recipe} />
+            {milk.map((r) => (
+              <RecipeCard recipe={r} />
+            ))}
+          </div>
+          <h3 className="recipe-slider-title">
+            {" "}
+            Recettes contenant de l&apos;herbe a chat{" "}
+          </h3>
+          <div className="recipes-slider">
+            {weed.map((r) => (
+              <RecipeCard recipe={r} />
+            ))}
+          </div>
+          <h3 className="recipe-slider-title"> Recettes healthy </h3>
+          <div className="recipes-slider">
+            {healthy.map((r) => (
+              <RecipeCard recipe={r} />
+            ))}
           </div>
         </div>
       </div>
